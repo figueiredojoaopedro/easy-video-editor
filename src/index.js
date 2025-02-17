c11507642c21c7200b61df4dfa7f6c9c7a3ce697
figueiredojoaopedro <neewprojecti@gmail.com>
2025-02-16 22:58:09 -0300

    merge mp4 files implemented at `index.js` and `src/processor/video.js`

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import video from "./processor/video.js";
+import dirs from "../utils/dirs.js";
+
+const parameters = process.argv.slice(2);
+
+const main = async () => {
+  try {
+    console.info("Invoked...", new Date().toISOString(), `\n`);
+
+    const processment = parameters[0];
+    const processment_params = parameters.slice(1);
+
+    console.info(`Processment: ${processment}`);
+
+    switch (processment) {
+      case "join":
+        await video.joiner(...processment_params);
+        break;
+      case "clear":
+        dirs.clear(...processment_params);
+        break;
+      default:
+        console.error("Invalid processment: " + processment);
+        break;
+    }
+  } catch (error) {
+    console.error(error);
+  } finally {
+    console.info("\nFinished...", new Date().toISOString());
+  }
+};
+
+main();
---
+++ src/processor/video.js
@@ -0,0 +1,107 @@
+import ffmpeg from "fluent-ffmpeg";
+import path from "path";
+import fs from "fs";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const input_dir = path.join(__dirname, "..", "..", "input");
+const output_dir = path.join(__dirname, "..", "..", "output");
+const preprocessed_dir = path.join(
+  __dirname,
+  "..",
+  "..",
+  "input",
+  "preprocessed"
+);
+
+// Create the output file path
+if (!fs.existsSync(output_dir)) {
+  fs.mkdirSync(output_dir, { recursive: true });
+}
+
+// Check if the output directory is writable
+try {
+  fs.accessSync(output_dir, fs.constants.W_OK);
+} catch (err) {
+  console.error("Error: No write access to output directory", err);
+  process.exit(1);
+}
+
+// preprocess files to convert them from fragmented mp4 to mp4
+const preprocessMp4 = (inputPath, outputPath) => {
+  return new Promise((resolve, reject) => {
+    ffmpeg(inputPath)
+      .outputOptions("-movflags +faststart") // Fix MP4 without re-encoding
+      .on("end", () => resolve(outputPath))
+      .on("error", (error) => reject(error))
+      .save(outputPath);
+  });
+};
+
+const joiner = async (gain = 1, source_format = "mp4") => {
+  const timestamp = new Date().getTime();
+
+  const output_filename = `output_joined_${timestamp}.mp4`;
+  const output_path = path.resolve(output_dir, output_filename);
+
+  // Ensure output directory exists
+  if (!fs.existsSync(output_dir)) {
+    fs.mkdirSync(output_dir, { recursive: true });
+  }
+
+  // Ensure preprocessed directory exists
+  if (!fs.existsSync(preprocessed_dir)) {
+    fs.mkdirSync(preprocessed_dir, { recursive: true });
+  }
+
+  let input_videos = fs
+    .readdirSync(input_dir)
+    .filter((file) => file.endsWith(".mp4"));
+
+  if (input_videos.length === 0) {
+    console.info("No input files found to join.");
+    return;
+  }
+
+  if (source_format !== "mp4") {
+    input_videos = await Promise.all(
+      input_videos.map((file, index) => {
+        const fixed_path = path.resolve(input_dir, file);
+
+        return preprocessMp4(
+          fixed_path,
+          path.resolve(input_dir, `preprocessed/${file}`)
+        );
+      })
+    );
+  } else {
+    input_videos = input_videos.map((file) => path.resolve(input_dir, file));
+  }
+
+  console.log("Input files:", input_videos + "\n");
+  console.log("Output Path:", output_path + "\n");
+
+  return new Promise((resolve, reject) => {
+    const command = ffmpeg({
+      source: input_videos[0],
+    });
+
+    input_videos.slice(1).forEach((input_video) => {
+      console.info(`Adding file: ${input_video}...`);
+      command.input(input_video);
+    });
+
+    command
+      .on("start", (cmd) => console.log("\nFFmpeg command:", cmd + "\n"))
+      .on("end", () => {
+        console.info("Merge complete:", output_filename);
+        resolve(output_filename);
+      })
+      .on("error", (error) => reject(error))
+      .mergeToFile(output_path);
+  });
+};
+
+export default {
+  joiner,
+};
